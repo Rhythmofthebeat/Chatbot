0802b240b0ce30a975ef805b92753e8360acf705
Rhythmofthebeat <138938275+Rhythmofthebeat@users.noreply.github.com>
2025-06-04 04:04:34 -0400

    Add files via upload

---
+++ dist/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { config } from 'dotenv';
+import morgan from 'morgan';
+import appRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+config();
+const app = express();
+//REQUESTS: in backend development
+// GET (get data)
+// PUT- Modify data
+// POST send data
+// DELETE send data to delete something
+//app.get("/hello", (req, res, next) => {
+//return res.send("Hello"); 
+//}); 
+//middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+//remove it in production
+app.use(morgan("dev"));
+app.use("/api/v1", appRouter);
+export default app;
+//# sourceMappingURL=app.js.map
